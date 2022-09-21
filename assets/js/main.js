fd446789457ea423827105e9e62eba6999a14bd3
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-09-21 22:31:00 +0600

    refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -233,4 +233,32 @@ muted.addEventListener("click", function () {
     muted.src = './assets/images/muted.svg'
   }
 });
+
+
+fraction = 0.5;
+function checkScroll() {
+
+    
+        var video = el;
+
+        var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
+            b = y + h, //bottom
+            visibleX, visibleY, visible;
+
+            visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+            visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+            visible = visibleX * visibleY / (w * h);
+
+            if (visible > fraction) {
+                video.play();
+            } else {
+                video.pause();
+            }
+
+}
+
+window.addEventListener('scroll', checkScroll, false);
+window.addEventListener('resize', checkScroll, false);
+
 /* ======================= Video Play End  =========================== */
