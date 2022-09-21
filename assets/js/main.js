8a47ac8e400c00da4c5e55d1cae5e6b11fdd68c0
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-09-21 17:55:24 +0600

    refactor code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,17 @@
 
 const navItems = document.querySelectorAll(".nav-item");
 const navbarContent = document.querySelector(".navbar-collapse");
+const navbarImg = document.querySelector(".navbar-img");
 
 navItems.forEach((el) => {
   el.addEventListener("click", function () {
     navbarContent.classList.remove("show");
+    navbarImg.classList.remove("active");
   });
 });
 
 const navbarToggler = document.querySelector(".navbar-toggler");
-const navbarImg = document.querySelector(".navbar-img");
+
 
 navbarImg.addEventListener("click", function () {
   navbarImg.classList.toggle("active");
